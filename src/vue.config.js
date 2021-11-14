eb248e3f4ff24d435481f656b15436917e13abc7
Julian Lau <julian.lau@aol.com>
2021-11-14 19:17:46 +0100

    theme color

--- src/vue.config.js
+++ src/vue.config.js
@@ -4,15 +4,12 @@ module.exports = {
     themeColor: '#DAF0EE',
     msTileColor: '#DAF0EE',
     backgroundColor: '#DAF0EE',
-    mobileWebAppCapable: 'yes',
+    appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'default',
-    start_url: '.',
-
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
+  },
+  workboxPluginMode: 'InjectManifest',
      workboxOptions: {
       swSrc: 'src/service-worker.js',
       exclude: [/\.map$/, /_redirects/],
     }
-  }
 }
