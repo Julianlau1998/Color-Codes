680a2093adc6420dac952acf743b998138c837a8
Julian Lau <julian.lau@aol.com>
2021-11-22 22:04:59 +0100

    config

--- src/vue.config.js
+++ src/vue.config.js
@@ -6,10 +6,11 @@ module.exports = {
     backgroundColor: '#DAF0EE',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'default',
-  },
-  workboxPluginMode: 'InjectManifest',
+    workboxPluginMode: 'InjectManifest',
      workboxOptions: {
       swSrc: 'src/service-worker.js',
       exclude: [/\.map$/, /_redirects/],
     }
+  },
 }
+  
