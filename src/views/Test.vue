81f3c092cc3915c31430a006ad627bed0dabb8d3
Julian Lau <julian.lau@aol.com>
2022-10-02 20:35:19 +0200

    test form

---
+++ src/views/Test.vue
@@ -0,0 +1,27 @@
+<template>
+  <form name="contact" netlify>
+      <br><br><br>
+    <p>
+        <label>Name <input type="text" name="name" /></label>
+    </p>
+    <p>
+        <label>Email <input type="email" name="email" /></label>
+    </p>
+    <p>
+        <label>Test <input type="text" name="text" /></label>
+    </p>
+    <p>
+        <button type="submit">Send</button>
+    </p>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
